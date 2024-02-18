91ab2d5e99292f79bddcaace9b140e58aa358b01
Ervic linares <ervic2015@gmail.com>
2024-02-18 00:57:19 -0500

    agregando contacto de acesores

---
+++ src/data/contacto.js
@@ -0,0 +1,14 @@
+export const contacto = [
+  {
+    name: "Ervic Linares",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_ozzwOE_6lsZXL_cP4euNJMZHxCUEfXbtmnIAUDSCSQ&s",
+    cel: 994322867,
+    id: 1,
+  },
+  {
+    name: "FAtima navarro",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_ozzwOE_6lsZXL_cP4euNJMZHxCUEfXbtmnIAUDSCSQ&s",
+    cel: 9000220202,
+    id: 2,
+  },
+];
