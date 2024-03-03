71e0d2994fac8c5a49104a23b3bd54c5c93e3b5f
Ervic linares <ervic2015@gmail.com>
2024-03-03 01:15:30 -0500

    creando las card de materiales

---
+++ src/data/card.js
@@ -0,0 +1,34 @@
+export const card = [
+  {
+    title: "Gyplac ST Extraliviana",
+    img: "https://static.eternit.pe/cdf/productos/prod_gyplac.jpg",
+    alt: "Plancha Standar",
+    parrafo:
+      "Nuestra placa de yeso con la misma calidad y resistencia de siempre",
+    inf: "https://static.eternit.pe/cdf/productos/pdf/ficha-tecnica-extraliviana-127mm.pdf",
+  },
+  {
+    title: "Gyplac RH Extraliviana - Resistente ...",
+    img: "https://static.eternit.pe/cdf/productos/gyplac-resistente-humedad.jpg",
+    alt: "Plancha Rh extraliviana",
+    parrafo:
+      "Gyplac RH Extraliviana es la placa de yeso resistente a la humedad",
+    inf: "https://static.eternit.pe/cdf/productos/pdf/FichatecnicaRHextraliviana.pdf",
+  },
+  {
+    title: "Superboard Texturizada",
+    img: "https://static.eternit.pe/cdf/productos/producto_20210426220524.jpg",
+    alt: "Plancha Texturizada",
+    parrafo:
+      "Placa de fibrocemento con textura madera que te ayuda a ahorrar tiempo y dinero",
+    inf: "https://static.eternit.pe/cdf/productos/pdf/ficha-tecnica-texturizada-_20210426225401.pdf",
+  },
+  {
+    title: "Superboard Paredes Exteriores",
+    img: "https://static.eternit.pe/cdf/productos/producto_20220405124936.jpg",
+    alt: "Superboard Paredes Exteriores",
+    parrafo:
+      "Nuestra placa de fibrocemento resistente a la humedad e impacto para la elaboración de pared...",
+    inf: "https://static.eternit.pe/cdf/productos/pdf/ficha-tecnica-paredes-exteriores-8-mm-final-3_20210518085246.pdf",
+  },
+];
